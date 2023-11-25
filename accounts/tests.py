74751d5df472b8b02b1f2a9d2ed00c7332d94012
jaemin.kim <jmkim648@gmail.com>
2023-11-25 17:47:37 +0900

    :sparkles: Feat: accounts > tests.py ( #5 )
    - 생성한 파일
    accounts 앱의 test 케이스 생성
    
    - 비고
    profile 테스트는 DRF 구현 시 api 엔디포인트를 가져와서 data에 포함되어 있는지 확인하는 식으로 변경

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='password',
+            nickname='testuser'
+        )
+        self.manager_user = get_user_model().objects.create_user(
+            email='manager@test.com',
+            password='password',
+            nickname='manager',
+            is_manager=True
+        )
+        self.client.login(email='test@test.com', password='password')
+        # self.client.login(email='manager@test.com', password='password')
+
+    # create test
+    def test_create_user(self):
+        '''user create test'''
+        print('===user create===')
+        self.assertEqual(self.user.email, 'test@test.com')
+        self.assertEqual(self.user.nickname, 'testuser')
+        self.assertTrue(self.user.is_active)
+
+    # profile 페이지 DRF 구현 시 수정 필요
+    def test_profile_view(self):
+        '''user profile test'''
+        print('===user profile===')
+        url = reverse('profile')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.user.nickname)
